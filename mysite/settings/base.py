d90572a93a862103705dc4a56bc839c44faf7aa2
son-content-mastery <contact@contentmastery.io>
2026-02-02 22:06:53 +0700

    feat: setup docker compose

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -83,7 +83,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': env.db('DATABASE_URL', default=f'sqlite:///{BASE_DIR}/db.sqlite3')
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+    }
 }
 
 
